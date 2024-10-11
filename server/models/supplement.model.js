4eab3475f8284deef563cc563b52432d828d7880
Samantha Danielle Reyes <samanthadaniellereyes.54@gmail.com>
2024-10-10 21:04:50 -0400

    New model for suppmenets

---
+++ server/models/supplement.model.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+const SupplementSchema = new mongoose.Schema ({
+    id: {
+        type: Number,
+        unique: true,
+        required: 'ID is required'
+    },
+    name: {
+        type: String,
+        required: "Name is required",
+    },
+    description: {
+        type: String,
+        required: "Description is required",
+    },
+    price: {
+        type: Number,
+        required: "Price is required",
+    },
+});
+
+export default mongoose.model('Supplement', SupplementSchema);
